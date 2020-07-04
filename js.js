8a651f79d7a44112bfdfaf502976b8dbecbd8016
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2020-07-04 17:48:59 +0200

    initial commit

---
+++ js.js
@@ -0,0 +1,49 @@
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+function lookUpProfile(name, prop) {
+    for (var i = 0; i < contacts.length; i++) {
+      if (contacts[i].firstName === name) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+  }
+
+lookUpProfile("Kristian", "lastName");
+lookUpProfile("Sherlock", "likes");
+lookUpProfile("Harry","likes");
+lookUpProfile("Bob", "potato");
+lookUpProfile("Akira", "address");
+
+
+
+
