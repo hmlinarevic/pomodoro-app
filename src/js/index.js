fb37846c1b556636d8b46d677175a2925afc881e
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2021-05-07 14:18:11 +0200

    fix icon url index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,11 @@ import { init } from './controller.js';
 import '../styles/css/style.css';
 import Icon from '../img/tomato.svg';
 
+console.log(Icon);
 init();
+
+const myIcon = new Image();
+myIcon.src = Icon;
+
+document.querySelector('.header__logo--icon').setAttribute('src', myIcon.src);
+document.querySelector('.footer__icon').setAttribute('src', myIcon.src);
